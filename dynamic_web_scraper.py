381f1330624031f1a8c2359411c452bc40b0d54d
woncook Lee <kjjrnr@naver.com>
2024-05-09 16:08:13 +0900

    여러 키워드 입력하게끔 변경

--- dynamic_web_scraper.py
+++ dynamic_web_scraper.py
@@ -3,57 +3,81 @@ import time
 from bs4 import BeautifulSoup
 import csv
 
-with sync_playwright() as playwright:
-    chromium = playwright.chromium
-    # 브라우저 안보이게 실행 = True
-    browser = chromium.launch(headless=False)
-    page = browser.new_page()
-    page.goto("https://www.wanted.co.kr/")
-    time.sleep(3)
+def wantedJobsScraper(keyword) :
+    jobs_db = []
+    print("wantedJobsScraper start")
+    with sync_playwright() as playwright:
+        chromium = playwright.firefox
+        # 브라우저 안보이게 실행 = True
+        browser = chromium.launch(headless=True)
+        page = browser.new_page()
+        page.goto("https://www.wanted.co.kr/")
+        time.sleep(5)
 
-    # 검색버튼 클릭
-    page.click("button.Aside_searchButton__Xhqq3")
-    time.sleep(1)
-
-    input = page.get_by_placeholder("검색어를 입력해 주세요.")
-    input.fill("java")
-
-    page.keyboard.down("Enter")
-    time.sleep(4)
-
-    page.click("a#search_tab_position")
-    time.sleep(4)
-
-    for i in range(8):
-        page.keyboard.down("End")
+        print("검색버튼 클릭 전")
+        
+        # 검색버튼 클릭
+        #print(page.content())
+        page.click("button.Aside_searchButton__Xhqq3")
         time.sleep(3)
 
-    content = page.content()
-    # print(content)
+        print("검색어를 입력 전")
+        input = page.get_by_placeholder("검색어를 입력해 주세요.")
+        input.fill(keyword)
+
+        print("엔터 전")
+        page.keyboard.down("Enter")
+        time.sleep(4)
+
+        print("포지션 클릭 전")
+        page.click("a#search_tab_position")
+        time.sleep(4)
+
+        for i in range(8):
+            page.keyboard.down("End")
+            time.sleep(3)
+
+        content = page.content()
+        # print(content)
+        
+        time.sleep(3)
+        playwright.stop()
+        
+        soup = BeautifulSoup(content, "html.parser")
+        
+        jobs = soup.find_all("div", class_="JobCard_container__FqChn")
+        
+        for job in jobs :
+            title = job.find("strong", class_="JobCard_title__ddkwM")
+            company = job.find("span", class_="JobCard_companyName__vZMqJ")
+            reward = job.find("span", class_="JobCard_reward__sdyHn")
+            link = job.find("a").get("href")
+            job_data = {
+                "keyword" : keyword,
+                "title" : title.text,
+                "company" : company.text,
+                "reward" : reward.text,
+                "link" : f"https://www.wanted.co.kr{link}",
+            }
+            jobs_db.append(job_data)
+        
     
-    time.sleep(3)
-    playwright.stop()
-    
-    soup = BeautifulSoup(content, "html.parser")
-    
-    jobs = soup.find_all("div", class_="JobCard_container__FqChn")
-    jobs_db = []
-    for job in jobs :
-        title = job.find("strong", class_="JobCard_title__ddkwM")
-        company = job.find("span", class_="JobCard_companyName__vZMqJ")
-        reward = job.find("span", class_="JobCard_reward__sdyHn")
-        link = job.find("a").get("href")
-        job_data = {
-            "title" : title.text,
-            "company" : company.text,
-            "reward" : reward.text,
-            "link" : f"https://www.wanted.co.kr{link}",
-        }
-        jobs_db.append(job_data)
-    
-    file = open("java_jobs.csv", "w", encoding="utf-8", newline = "")
-    writer = csv.writer(file)
-    writer.writerow(["title", "company", "reward", "link"])
+    print("wantedJobsScraper end ", jobs_db)
+    return jobs_db
+
+keywords = [
+    "java",
+    "react",
+]
+
+file = open("jobs.csv", "w", encoding="utf-8", newline = "")
+writer = csv.writer(file)
+writer.writerow(["keyword", "title", "company", "reward", "link"])
+
+for keyword in keywords :
+    jobs_db = wantedJobsScraper(keyword)
     
     for job_data in jobs_db :
         writer.writerow(job_data.values())
+
+file.close()
